0abeea71a15b4c9a85ca8577159d783f32363fb9
divyeshdobariya <divyeshdobariya2593@gmail.com>
2022-01-26 17:06:41 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get update;
+sudo apt-get upgrade;
+sudo apt-get install apache2;
+sudo service apache2 restart;
+sudo add-apt-repository ppa:ondrej/php;
+sudo apt-get install php7.4;
+sudo apt-get install libapache2-mod-php7.4;
+
